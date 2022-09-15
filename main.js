60522162b8e208d5622be441cd2e258827dce8e8
Youme <rosydarling@naver.com>
2022-09-15 23:17:21 +0100

    conditional rendering

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ const app = Vue.createApp({
         return {
             product: 'socks',
             description: 'It makes your foot warm.',
-            image: './assets/images/socks_green.jpg',
+            image1: './assets/images/socks_green.jpg',
+            image2: './assets/images/socks_blue.jpg',
             url: 'https://youmeson.github.io/portfolio/',
+            onSaleGreen: true,
+            inventoryGreen: 100,
+            onSaleBlue: false,
+            inventoryBlue: 0,
         }
     }
 
